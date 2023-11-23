bf107e46ca5a8626873b65563842c159297dc54e
AMirreh17 <57352379+AMirreh17@users.noreply.github.com>
2023-11-23 22:19:18 +0000

    final changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider, createTheme } from '@mui/material';
-import { BrowserRouter, HashRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 
 const theme = createTheme(
