6e51c38e0ea73eb4bfd4714d96ae3397aa504de5
AMirreh17 <57352379+AMirreh17@users.noreply.github.com>
2024-01-31 14:53:38 +0000

    changes to homepage

--- src/Home.js
+++ src/Home.js
@@ -32,48 +32,7 @@ export default function Home() {
         <br></br>
         <Button variant='contained' sx={{mt: 2}} href="/CCTP/#about">Learn More</Button>
       </header>
-
       
-      
-      <div className='mainBody'>
-        <div className='wrapper1'>
-        <Box sx={{display: 'flex', flexDirection: {xs: "column", md: "row"}, justifyContent:'space-between' }}>
-      <Paper elevation={16} square={false} style={{ width: {xs: 1, md:500}, padding: 30 }}>
-    
-      <div className='text'>
-      <Typography variant='h2' sx={{my: 1, mt: 2}}>Welcome our website</Typography>
-      <Typography variant='h3'>
-          *Prototype Version*
-        </Typography>
-       
-        <Typography variant= 'body2' sx={{my: 4, mt: 2, whiteSpace: 'pre-line'}} > Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
-          <Typography variant= 'body2'  sx={{my: 4, mt: 2}}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
-            <br></br>
-          {
-
-            //<img src={welcomeimg} width="500" height="500"alt=''/>
-          }
-           
-          
-            
-        </div> 
-         
-         </Paper>
-        
-          </Box>
-          
-          </div>
-      </div> 
-     
-
-
-
       <div className='mainBody'>
         <Box sx={{display: 'flex', flexDirection: {xs: "column", md: "row"},  justifyContent:'space-between', gap:3, m: 3,  }}>
        
