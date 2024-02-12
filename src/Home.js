6bba2f24c895b556b6693f3a1900786e84371c20
AMirreh17 <57352379+AMirreh17@users.noreply.github.com>
2024-02-12 18:43:02 +0000

    implementing scroll bar and changes to home page

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import './App.css';
 
 import { /*Box, Button,  Paper,*/ Typography, Grid } from '@mui/material';
 import NavBar from './NavBar';
-import {motion, useScroll, useTransform} from 'framer-motion';
+import {motion, useMotionValueEvent, useScroll, useTransform, useSpring, useMotionValue} from 'framer-motion';
 
 import welcomeimg from '../src/assets/image4.png';
 import welcomeimg2 from '../src/assets/Image5.png';
@@ -31,6 +31,7 @@ const fadeInAnimationsVariants = {
 export default function Home() {
 
       const container = useRef(null);
+      const yScroll = useMotionValue(0);
       const {scrollYProgress} = useScroll({
         target: container,
         offset: ['start end', 'end start']
@@ -39,14 +40,25 @@ export default function Home() {
       )
       
       const sm = useTransform(scrollYProgress, [0,1], [0,-50]);
-      const md = useTransform(scrollYProgress, [0,1], [0,-150]);
+     
+      const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001
+      });
+      
 
-
+      useMotionValueEvent(scrollYProgress, "change", (latest) => {
+        yScroll.set(latest);
+        console.log(latest)
+      })
 
   return (
         <div ref={container} className="App">
+      
       <NavBar />
       
+     
       
       <header className="App-header"> 
       {
@@ -57,16 +69,16 @@ export default function Home() {
 
         
         
-        <motion.h1  class="text-7xl font-bold leading-normal"> The Undecover Project</motion.h1>
         
+        <Typography fontSize="80px" variant='h1' sx={{my: 4, }}>The Undecover Project</Typography>
         {
-         /* <Typography fontSize="80px" variant='h1' sx={{my: 4, }}>The Undecover Project</Typography>
+         /* <motion.h1  class="text-7xl font-bold leading-normal"> The Undecover Project</motion.h1>
            <Typography variant='h6'>
           Revealing the truth behind Artificial Intelligence within social media content and platforms
         </Typography>*/
         }
          
-        <motion.p style={{y:sm}} class="font-light  max-w-xl">Revealing the truth behind Artificial Intelligence within social media content and platforms</motion.p>
+        <motion.p class="font-light  max-w-xl">Revealing the truth behind Artificial Intelligence within social media content and platforms</motion.p>
         <br></br>
         
        
@@ -79,7 +91,7 @@ export default function Home() {
         
         
         <a href='/CCTP/#about'>
-        <motion.button style={{y:md}} whileHover={{scale: 1.2}} whileTap={{scale: 0.9}} class="bg-white px-10 py-5 text-xl uppercase tracking-widest text-black rounded-full" href="/CCTP/#about"> Learn More</motion.button>
+        <motion.button style={{y:sm}} whileHover={{scale: 1.2}} whileTap={{scale: 0.9}} class="bg-white px-10 py-5 text-xl uppercase tracking-widest text-black rounded-full" href="/CCTP/#about"> Learn More</motion.button>
         </a>
         
         
@@ -87,7 +99,7 @@ export default function Home() {
         
       </header>
       
-      <motion.div class="text-white">
+      <motion.div class="text-white py-10">
 
         <motion.div variants={fadeInAnimationsVariants} initial="initial" whileInView="animate" viewport={{once: true,}}>
       <Grid container direction="row" my={4} className='text' spacing={2} columnSpacing={21}>
@@ -116,7 +128,7 @@ export default function Home() {
       </motion.div>
       </motion.div>
       
-      <motion.div className="" >
+      <motion.div className="" class="py-10">
       <motion.div variants={fadeInAnimationsVariants} initial="initial" whileInView="animate" viewport={{once: true,}}>
       <Grid container direction="row" my={4} className='text2' spacing={2} columnSpacing={21}>
       <Grid item>
@@ -127,9 +139,9 @@ export default function Home() {
             <br></br>
           <div className="textColor">
         <Typography  fontSize="20px" sx={{mt: 2}} > 
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+        Our mission is to bring further awareness to AI within the context of social media. 
+        This online resource will help bring clarity to how AI is used within social media platforms as well as how social media users utilise AI tools to produce social media based content.
+        We want to use this resource to raise future discussions on AI
         </Typography>
         <br></br>
         </div>
@@ -143,13 +155,13 @@ export default function Home() {
       <Grid container direction="row" my={4} className='text3' spacing={2} columnSpacing={21}>
        
         <Grid item xs={11} sm={6}>
-        <Typography fontSize="50px"variant='h2' sx={{my: 1, mt: 2}}>Is AI bad for social media?</Typography>
+        <Typography fontSize="50px"variant='h2' sx={{my: 1, mt: 2}}>Does AI Impact our social media experience?</Typography>
             <br></br>
           <div className="textColor">
         <Typography  fontSize="20px" sx={{mt: 2}} > 
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged 
+        All social media applications utilise AI algorithms in some form to improve their overall user experiences. 
+        AI allows social media applications to understand their user’s habits more such as how users interact with different 
+        genres of content and recommending potential content which they might be interested in. 
         </Typography>
         <br></br>
         </div>
@@ -167,9 +179,9 @@ export default function Home() {
             <br></br>
           <div className="textColor">
         <Typography  fontSize="20px" sx={{mt: 2}} > 
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged 
+        AI technology has developed further within the recent decades; however, we still are uncertain on the growth of AI and it’s impacts on our livelihood. 
+        The “negative stigma” behind AI stems from the dangers and risks which AI can bring to our society in the future. 
+        For example, Privacy issues, Legal implications and Employability. 
         </Typography>
         <br></br>
         </div>
@@ -187,9 +199,8 @@ export default function Home() {
             <br></br>
           <div className="textColor">
         <Typography  fontSize="20px" sx={{mt: 2}} > 
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged 
+        There are various AI related tools which can improve the productivity and creation of social media content. 
+        Some AI tools available can aid in content ideation, video editing, content distribution and many more.
         </Typography>
         <br></br>
         </div>
@@ -198,12 +209,15 @@ export default function Home() {
       </motion.div>
       </motion.div>
 
-      <motion.div className="welcome3">
+      
+
+      <motion.div className="welcome3" class="text-white">
       <motion.div variants={fadeInAnimationsVariants} initial="initial" whileInView="animate" viewport={{once: true,}}>
+      
       <Grid container direction="row" my={4} className='text3' spacing={2} columnSpacing={21}>
       
-        <Grid item xs={11} sm={4}>
-        <Typography fontSize="50px"variant='h2' sx={{my: 1, mt: 2}}>About us</Typography>
+      <Grid item xs={11} sm={8} md={4}>
+        <Typography fontSize="50px"variant='h2' sx={{my: 1, mt: 2}}>About </Typography>
             <br></br>
           <div className="textColor">
         <Typography  fontSize="20px" sx={{mt: 2}} > 
@@ -212,11 +226,11 @@ export default function Home() {
         <br></br>
         </div>
         <a href='/CCTP/#about'>
-        <motion.button whileHover={{scale: 1.2}} whileTap={{scale: 0.9}} class=" bg-white px-10 py-5 text- uppercase tracking-widest text-black rounded-full" href="/CCTP/#about"> Learn More</motion.button>
+        <motion.button whileHover={{scale: 1.2}} whileTap={{scale: 0.9}} class=" bg-white px-7 py-5 text- uppercase tracking-widest text-black rounded-full" href="/CCTP/#about"> Learn More</motion.button>
         </a>
         </Grid>
-        <Grid item xs={11} sm={4}>
-        <Typography fontSize="50px"variant='h2' sx={{my: 1, mt: 2}}>What is AI?</Typography>
+        <Grid item xs={11} sm={8} md={4}>
+        <Typography fontSize="50px"variant='h2' sx={{my: 1, mt: 2}}>Background</Typography>
             <br></br>
           <div className="textColor">
         <Typography  fontSize="20px" sx={{mt: 2}} > 
@@ -225,11 +239,11 @@ export default function Home() {
         <br></br>
         </div>
         <a href='/CCTP/#background'>
-        <motion.button whileHover={{scale: 1.2}} whileTap={{scale: 0.9}} class=" bg-white px-10 py-5 text- uppercase tracking-widest text-black rounded-full" href="/CCTP/#about"> Learn More</motion.button>
+        <motion.button whileHover={{scale: 1.2}} whileTap={{scale: 0.9}} class=" bg-white px-7 py-5 text- uppercase tracking-widest text-black rounded-full" href="/CCTP/#about"> Learn More</motion.button>
         </a>
         </Grid>
-        <Grid item xs={11} sm={4}>
-        <Typography fontSize="50px"variant='h2' sx={{my: 1, mt: 2}}>Popular AI Tools</Typography>
+        <Grid item xs={11} sm={8} md={4}>
+        <Typography fontSize="50px"variant='h2' sx={{my: 1, mt: 2}}>Tools</Typography>
             <br></br>
           <div className="textColor">
         <Typography  fontSize="20px" sx={{mt: 2}} > 
@@ -238,7 +252,7 @@ export default function Home() {
         <br></br>
         </div>
         <a href='/CCTP/#recommendations'>
-        <motion.button whileHover={{scale: 1.2}} whileTap={{scale: 0.9}}class=" bg-white px-10 py-5 text- uppercase tracking-widest text-black rounded-full" href="/CCTP/#about"> Learn More</motion.button>
+        <motion.button whileHover={{scale: 1.2}} whileTap={{scale: 0.9}}class=" bg-white px-7 py-5 text- uppercase tracking-widest text-black rounded-full" href="/CCTP/#about"> Learn More</motion.button>
         </a>
         </Grid>
       </Grid>
@@ -317,6 +331,17 @@ export default function Home() {
           ©The Undercover Project - 2024
           </Typography>
         </footer>
+        <motion.div style={{
+          scaleX,
+        background: "white",
+        transformOrigin: "Left",
+        position: "sticky",
+        top: 0,
+        bottom: 0,
+        width: "100%",
+        height: "5px"
+      }}>
+      </motion.div>
     </div>
     );
 
