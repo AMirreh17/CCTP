a034ec4e94f72fcd185b08333b3e4840fe212092
AMirreh17 <57352379+AMirreh17@users.noreply.github.com>
2024-02-28 18:41:31 +0000

    edits to the navbar and image styling

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,6 @@
 import { AppBar, IconButton, Stack, Toolbar, Typography, Button, Drawer, Box} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
-//import AppLogo from '../src/assets/logo2.png';
+import AppLogo from '../src/assets/logo2.png';
 
 import {useState} from 'react'
 
@@ -8,27 +8,29 @@ import {useState} from 'react'
 export default function NavBar() {
    const [isDrawerOpen, setIsDrawerOpen] = useState(false)
     return (
-<AppBar position="static" style={{background: 'transparent'}}>
+<AppBar position="sticky" style={{background: '#111111', minHeight: "70px"}}>
    
  <Toolbar>
     <IconButton size='large' edge='start' color='inherit' aria-label="logo" onClick={() => setIsDrawerOpen(true)}>
-     {
-      //<img src={AppLogo} width="40" height="40" alt=""/>
-     }   
+     
+      
+        
     <MenuIcon />
     </IconButton>
+    <img src={AppLogo} width="40" height="40" alt=""/>
     <Typography variant="h6"></Typography>
     <Typography variant='h6' component='div' sx={{flexGrow: 1}}>
      
     </Typography>
     <Stack direction='row' spacing={2}>
+
+      <Button color='inherit' href="/CCTP/#background">Background</Button>
+       <Button color='inherit' href="/CCTP/#recommendations">Tools</Button>
+       <Button color='inherit' href="">Awareness</Button>
       {
        //<Button color='inherit' href="/CCTP">Home</Button>
        //<Button color='inherit' href="/CCTP/#about">About</Button>
-       //<Button color='inherit' href="/CCTP/#background">Background</Button>
-       //<Button color='inherit' href="/CCTP/#recommendations">Recommendations</Button>
-       
-        //<Button color='inherit' href="/CCTP/#showcase">Showcase</Button>
+      //<Button color='inherit' href="/CCTP/#showcase">Showcase</Button>
        }
     </Stack>
    <Drawer anchor="left" open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
